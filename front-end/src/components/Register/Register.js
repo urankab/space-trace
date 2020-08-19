b76fdc0010ba6ed593b9f4d4d4869b2b9372e5b6
Uranka <39204253+urankab@users.noreply.github.com>
2020-08-19 16:09:42 -0600

    Playing with Cognito

--- front-end/src/components/Register/Register.js
+++ front-end/src/components/Register/Register.js
@@ -67,75 +67,75 @@ export default function Register(props) {
 
 // COGNITO STUFF
 
-  // const clearErrorState = () => {
-  //   setCogMsg('')
-  // }
+  const clearErrorState = () => {
+    setCogMsg('')
+  }
 
-  // const handleSubmit = async (e) => {
-  //   clearErrorState()
-  //   const { firstName, lastName, phone, email, password } = userForm;
-  //   let username = email
-  //   try {
-  //     const signUpResponse = await Auth.signUp({
-  //       username,
-  //       password,
-  //       // attributes: {
-  //       //   firstName: firstName,
-  //       //   lastName: lastName,
-  //       //   phone: phone,
-  //       // }
-  //     });
-  //     // data = await postData(postUrl, form);
-  //     // props.onLoginSuccess(data);
-  //     // this.props.history.push("/welcome");
-  //     console.log(signUpResponse);
-  //   } catch (error) {
-  //     // let err = null
-  //     // !error.message ? err = {"message": error}
-  //     setCogMsg(error)
-  //   }
-  // }
+  const handleSubmit = async (e) => {
+    clearErrorState()
+    const { firstName, lastName, phone, email, password } = userForm;
+    let username = email
+    try {
+      const signUpResponse = await Auth.signUp({
+        username,
+        password,
+        // attributes: {
+        //   firstName: firstName,
+        //   lastName: lastName,
+        //   phone: phone,
+        // }
+      });
+      // data = await postData(postUrl, form);
+      // props.onLoginSuccess(data);
+      // this.props.history.push("/welcome");
+      console.log(signUpResponse);
+    } catch (error) {
+      // let err = null
+      // !error.message ? err = {"message": error}
+      setCogMsg(error)
+    }
+  }
 
   // console.log(cogMsg)
 
 // API STUFF
 
-  const handleSubmit = async() => {
-    const { firstName, lastName, ...form } = userForm;
-    form.fullName = `${firstName} ${lastName}`;
-    let test = { message: '', firstName: '', lastName: '', email: '', phone: '', password: '' }
-    let isValid = true
+  // const handleSubmit = async() => {
+  //   const { firstName, lastName, ...form } = userForm;
+  //   form.fullName = `${firstName} ${lastName}`;
+  //   let test = { message: '', firstName: '', lastName: '', email: '', phone: '', password: '' }
+  //   let isValid = true
 
-    if (!userForm.firstName) {
-      test.firstName = 'Please enter your first name'
-      isValid = false
-    }
-    if (!userForm.lastName) {
-      test.lastName = 'Please enter your last name'
-      isValid = false
-    }
-    if (!validateEmail(userForm.email)) {
-      console.log('validating email')
-      console.log(validateEmail(userForm.email))
-      test.email = 'Please enter an e-mail'
-      isValid = false
-    }
-    if (!userForm.phone) {
-      test.phone = 'Please enter your phone number'
-      isValid = false
-    }
-    if (!validatePass(userForm.password, 8)) {
-      test.password = 'Password must be at least 8 characters'
-      isValid = false
-    }
-    setErrorMsg(test)
-    if (isValid) {
-      let data;
-      data = await postData(postUrl, form);
-      console.log(data['email']);
-      props.onLoginSuccess(data);
-    }
-  };
+  //   if (!userForm.firstName) {
+  //     test.firstName = 'Please enter your first name'
+  //     isValid = false
+  //   }
+  //   if (!userForm.lastName) {
+  //     test.lastName = 'Please enter your last name'
+  //     isValid = false
+  //   }
+  //   if (!validateEmail(userForm.email)) {
+  //     console.log('validating email')
+  //     console.log(validateEmail(userForm.email))
+  //     test.email = 'Please enter an e-mail'
+  //     isValid = false
+  //   }
+  //   if (!userForm.phone) {
+  //     test.phone = 'Please enter your phone number'
+  //     isValid = false
+  //   }
+  //   if (!validatePass(userForm.password, 8)) {
+  //     test.password = 'Password must be at least 8 characters'
+  //     isValid = false
+  //   }
+  //   setErrorMsg(test)
+  //   if (isValid) {
+  //     let data;
+  //     data = await postData(postUrl, form);
+  //     console.log(data['email']);
+  //     props.onLoginSuccess(data);
+  //   }
+  // };
 
 
   return (
@@ -148,7 +148,7 @@ export default function Register(props) {
         <Typography component="h1" variant="h5">
           Sign up
         </Typography>
-        {/* <p>{cogMsg.message}</p> */}
+        <p>{cogMsg.message}</p>
         <form
           className={classes.form}
           noValidate
